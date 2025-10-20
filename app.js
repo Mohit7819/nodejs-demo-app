73e90f1903499db82bb7e53e34c83a40b31208e3
Mohit Singh <m.schoudhary3050@gmail.com>
2025-10-20 21:45:37 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD automated Node.js App 🚀');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
